f325890f07a1dd9b8f3c94bbf10d7707d5eeb276
DeanRoos <tug91912@temple.edu>
2023-09-27 10:16:19 -0400

    flags working

--- MYps.c
+++ MYps.c
@@ -1,1 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+
+struct Flags{
+  int p;
+  int s;
+  int U;
+  int S;
+  int v;
+  int c;
+} options;
+
+int main(int argc, char *argv[]){
+
+  options.p = 0;
+  options.s = 0;
+  options.U = 0;
+  options.S = 0;
+  options.v = 0;
+  options.c = 0;
+
+  int flag;
+
+  while ((flag = getopt(argc, argv, "psUSvc")) != -1){
+    switch (flag){
+
+      case 'p' : options.p = 1;
+      break;
+      case 's' : options.s = 1;
+      break;
+      case 'U' : options.U = 1;
+      break;
+      case 'S' : options.S = 1;
+      break;
+      case 'v' : options.v = 1;
+      break;
+      case 'c' : options.c = 1;
+      break;
+
+    }
+  }
+
+  printf("options are:\np = %d\ns = %d\nU = %d\nS = %d\nv = %d\nc = %d", options.p, options.s, options.U, options.S, options.v, options.c);
+  return 0;
+
+}
