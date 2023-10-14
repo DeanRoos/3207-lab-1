1ef7343aec970e5058a4aec6e63c8edf9e01c974
DeanRoos <143642744+DeanRoos@users.noreply.github.com>
2023-10-13 22:58:54 -0400

    Created functions for creating dir paths

--- MYps.c
+++ MYps.c
@@ -1,19 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <getopt.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
 
+//functions to easily create filepath to stat, statm, and cmdline files
+char *catStat(const char *a);
+char *catStatm(const char *a);
+char *catCmdline(const char *a);
+
+//struct for the flag options from command line
 struct Flags{
   int p;
   int s;
   int U;
   int S;
-    int v;
+  int v;
   int c;
   char *PID;
 } options;
 
 int main(int argc, char *argv[]){
 
+  //setting all flags default to 0, 1 would mean that I was selected
   options.p = 0;
   options.s = 0;
   options.U = 0;
@@ -24,10 +34,12 @@ int main(int argc, char *argv[]){
 
   int flag;
 
+  //looping through args tyo find flags
   while ((flag = getopt(argc, argv, "p:sUSvc")) != -1){
     switch (flag){
 
       case 'p' : options.p = 1;
+      //setting pid if -p is used
       options.PID = optarg;
       break;
       case 's' : options.s = 1;
@@ -40,13 +52,18 @@ int main(int argc, char *argv[]){
       break;
       case 'c' : options.c = 1;
       break;
+      default:
+      printf("%s", "Invalid option. Exiting program.");
+      return 1;
 
     }
   }
 
+  //creating file
   FILE *fp;
 
-  fp = fopen("/proc/11/stat", "r");
+  //opting proc folder
+  fp = fopen("/proc", "r");
 
   //error checking on open
       if (fp == NULL){
@@ -54,16 +71,45 @@ int main(int argc, char *argv[]){
         return 1;
       }
 
-  char line[150];
+  //printing commands no -p argument
 
-  while (!feof(fp)){
-          fgets(line, 150, fp);
-          puts(line);
-  }
+  char *newName = catStat(options.PID);
+  printf("%s", newName);
+  newName = catStatm(options.PID);
+  printf("%s", newName);
+  newName = catCmdline(options.PID);
+  printf("%s", newName);
+  free(newName);
+
+  printf("%d\n", getuid());
 
   fclose(fp);
 
   printf("options are:\np = %d\nPID = %s\ns = %d\nU = %d\nS = %d\nv = %d\nc = %d\n", options.p, options.PID, options.s, options.U, options.S, options.v, options.c);
   return 0;
 
+}
+
+char *catStat(const char *a) {
+	char *ret = malloc(strlen(a) + 12);
+	strcpy(ret, "/proc/");
+  strcat(ret, a);
+  strcat(ret, "/stat\0");
+	return ret; //returning
+}
+
+char *catStatm(const char *a) {
+	char *ret = malloc(strlen(a) + 13);
+	strcpy(ret, "/proc/");
+  strcat(ret, a);
+  strcat(ret, "/statm\0");
+	return ret; //returning
+}
+
+char *catCmdline(const char *a) {
+	char *ret = malloc(strlen(a) + 15);
+	strcpy(ret, "/proc/");
+  strcat(ret, a);
+  strcat(ret, "/statm\0");
+	return ret; //returning
 }
