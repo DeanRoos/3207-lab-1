ae7e55bc3d043bc707f495caf7c51c39f7c81e1b
DeanRoos <143642744+DeanRoos@users.noreply.github.com>
2023-10-16 00:32:08 -0400

    dispCMD and dispState

--- MYps.c
+++ MYps.c
@@ -4,11 +4,18 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/stat.h>
 
 //functions to easily create filepath to stat, statm, and cmdline files
-char *catStat(const char *a);
-char *catStatm(const char *a);
-char *catCmdline(const char *a);
+char *catStat(const char *pid);
+char *catStatm(const char *pid);
+char *catCmdline(const char *pid);
+
+//functions for displaying the various options
+void dispCMD(const char *pid);
+void dispState(const char *pid);
 
 //struct for the flag options from command line
 struct Flags{
@@ -85,31 +92,91 @@ int main(int argc, char *argv[]){
 
   fclose(fp);
 
-  printf("options are:\np = %d\nPID = %s\ns = %d\nU = %d\nS = %d\nv = %d\nc = %d\n", options.p, options.PID, options.s, options.U, options.S, options.v, options.c);
-  return 0;
+  dispCMD(options.PID);
+  puts("");
+  dispState(options.PID);
+
+  // printf("options are:\np = %d\nPID = %s\ns = %d\nU = %d\nS = %d\nv = %d\nc = %d\n", options.p, options.PID, options.s, options.U, options.S, options.v, options.c);
+  // return 0;
 
 }
 
-char *catStat(const char *a) {
-	char *ret = malloc(strlen(a) + 12);
+char *catStat(const char *pid) {
+	char *ret = malloc(strlen(pid) + 12);
 	strcpy(ret, "/proc/");
-  strcat(ret, a);
+  strcat(ret, pid);
   strcat(ret, "/stat\0");
 	return ret; //returning
 }
 
-char *catStatm(const char *a) {
-	char *ret = malloc(strlen(a) + 13);
+char *catStatm(const char *pid) {
+	char *ret = malloc(strlen(pid) + 13);
 	strcpy(ret, "/proc/");
-  strcat(ret, a);
+  strcat(ret, pid);
   strcat(ret, "/statm\0");
 	return ret; //returning
 }
 
-char *catCmdline(const char *a) {
-	char *ret = malloc(strlen(a) + 15);
+char *catCmdline(const char *pid) {
+	char *ret = malloc(strlen(pid) + 15);
 	strcpy(ret, "/proc/");
-  strcat(ret, a);
-  strcat(ret, "/statm\0");
+  strcat(ret, pid);
+  strcat(ret, "/cmdline\0");
 	return ret; //returning
+}
+
+//function to display the cmdline 
+void dispCMD(const char *pid){
+
+  //uses catCmdline funtions to create file path name
+  char *path = catCmdline(pid);
+  //creating a buffer array to hold content of /proc/<pid>/cmdline
+  char buf[1024];
+
+  //reading from cmdline
+  int fp = open(path, O_RDONLY);
+  //getting length of file
+  size_t length = read(fp, buf, sizeof (buf));
+  //closing file
+  close (fp);
+
+  //null-terminating the end of the buffer so they know its the end of a string
+  buf[length] = '\0';
+  //string variable for each individual null-terminated part of the cmdline file
+  char *arg;
+
+  //looping through the file and printing each null-terminated string 
+  arg = buf;
+  while (arg < buf + length) {
+    printf ("%s\n", arg);
+    arg += strlen (arg) + 1;
+  }
+
+}
+
+void dispState(const char *pid){
+
+  //using catStat function to get file path for stat file
+  char *path = catStat(pid);
+  //creating a buffer to read the file into
+  char buf[1024];
+
+  //reading from stat
+  int fp = open(path, O_RDONLY);
+  //reading file
+  read(fp, buf, sizeof (buf));
+  //closing file
+  close (fp);
+
+  //creating a token strings to parse through the stat file 
+  char *token = strtok(buf, " ");
+
+  //because the state is always the 3rd, this goes through 3 times
+  for (size_t i = 0; i < 2; i++){
+    token = (strtok(NULL, " "));
+  }
+
+  //prints the token
+  printf("%s", token);
+
 }
